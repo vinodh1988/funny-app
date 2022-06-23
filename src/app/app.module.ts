c41c9dc03e13f93f2241b124604a10a6e3bc5869
vinodh1988 <k.c.vinodh@hotmail.com>
2022-06-23 15:44:49 +0530

    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoxComponent } from './box/box.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BoxComponent //component,directives,pipes
+    BoxComponent,
+    FooterComponent //component,directives,pipes
   ],
   imports: [
     BrowserModule, //include other modules
