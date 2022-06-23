81ff2e044f85d247a9361fd0e0eb86f1576846a0
vinodh1988 <k.c.vinodh@hotmail.com>
2022-06-23 15:20:07 +0530

    child-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoxComponent } from './box/box.component';
 
 @NgModule({
   declarations: [
-    AppComponent //component,directives,pipes
+    AppComponent,
+    BoxComponent //component,directives,pipes
   ],
   imports: [
     BrowserModule, //include other modules
